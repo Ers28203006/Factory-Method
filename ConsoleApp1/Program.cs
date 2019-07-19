af2802aeceddc89be3f57cb42290c05c05dc6c28
Ers28203006 <41695107+Ers28203006@users.noreply.github.com>
2019-07-19 11:11:01 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    public interface IPrint
+    {
+        void Print();
+    }
+
+    abstract class PrinterCreater
+    {
+        public string Name { get; set; }
+        public PrinterCreater(string name)
+        {
+            Name = name;
+            PrintName();
+        }
+        public abstract IPrint PrintMethod();
+
+        void PrintName()
+        {
+            Console.Write($"{Name} ");
+        }
+    }
+
+    class Canon : PrinterCreater
+    {
+        public Canon(string name) : base(name) { }
+
+        public override IPrint PrintMethod()
+        {
+            return new Perl();
+        }
+    }
+
+    class HP : PrinterCreater
+    {
+        public HP(string name) : base(name) { }
+
+        public override IPrint PrintMethod()
+        {
+            return new Offset();
+        }
+    }
+
+    class Perl : IPrint
+    {
+        public Perl()
+        {
+            Print();
+        }
+        public void Print()
+        {
+            Console.WriteLine("печатает бумагой Perl");
+        }
+    }
+
+    class Offset : IPrint
+    {
+        public Offset()
+        {
+            Print();
+        }
+        public void Print()
+        {
+            Console.WriteLine("печатает бумагой Offset");
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrinterCreater printerCreater = null;
+
+            printerCreater = new Canon("Canon");
+            printerCreater.PrintMethod();
+
+            printerCreater = new HP("HP");
+            printerCreater.PrintMethod();
+           
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    abstract class WindowCreater
+    {
+        public string Name{get;set;}
+
+        public WindowCreater(string name)
+        {
+            Name = name;
+        }
+
+        public abstract IOpenWindow OpenMethod();
+    }
+
+
+    class Windows : WindowCreater
+    {
+        public Windows(string name) : base(name) {}
+
+        public override IOpenWindow OpenMethod()
+        {
+            return new Native();
+        }
+    }
+
+    class Browser : WindowCreater
+    {
+        public Browser(string name) : base(name) { }
+
+        public override IOpenWindow OpenMethod()
+        {
+            return new HTML();
+        }
+    }
+
+    public interface IOpenWindow
+    {
+        string Open();
+    }
+
+    class Native : IOpenWindow
+    {
+        public string Open()
+        {
+           return "открывается окно Native";
+        }
+    }
+
+    class HTML : IOpenWindow
+    {
+        public string Open()
+        {
+            return "открывается окно HTML";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WindowCreater window = null;
+            IOpenWindow open = null; 
+
+            window = new Windows("Windows");
+            open = new Native();
+
+            Console.WriteLine($"В {window.Name} {open.Open()}");
+
+            open = new HTML();
+            window = new Windows("Browser");
+            Console.WriteLine($"В {window.Name} {open.Open()}");
+            Console.ReadKey();
+        }
+    }
+}
